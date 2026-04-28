1e9f4bd7a598494ce888f893a9dd4e82b95e3902
Caleb Fringer <cfringer98@gmail.com>
2026-04-28 12:34:10 -0700

    Add ClientCapabilities, General (client capabilities), PositionEncodingKinds types.

--- internal/lsp/initialize.go
+++ internal/lsp/initialize.go
@@ -6,12 +6,55 @@ type InitializeRequest struct {
 }
 
 type InitializeRequestParams struct {
-	ClientInfo *ClientInfo `json:"clientInfo"`
+	ClientInfo         *ClientInfo        `json:"clientInfo"`
+	ClientCapabilities ClientCapabilities `json:"capabilities"`
 }
+
 type ClientInfo struct {
 	Name    string `json:"name"`
 	Version string `json:"version"`
 }
+
+type ClientCapabilities struct {
+	/**
+	 * General client capabilities.
+	 *
+	 * @since 3.16.0
+	 */
+	General *General `json:"general"`
+}
+
+type General struct {
+	/**
+	 * The position encodings supported by the client. Client and server
+	 * have to agree on the same position encoding to ensure that offsets
+	 * (e.g. character position in a line) are interpreted the same on both
+	 * side.
+	 *
+	 * To keep the protocol backwards compatible the following applies: if
+	 * the value 'utf-16' is missing from the array of position encodings
+	 * servers can assume that the client supports UTF-16. UTF-16 is
+	 * therefore a mandatory encoding.
+	 *
+	 * If omitted it defaults to ['utf-16'].
+	 *
+	 * Implementation considerations: since the conversion from one encoding
+	 * into another requires the content of the file / line the conversion
+	 * is best done where the file is read which is usually on the server
+	 * side.
+	 *
+	 * @since 3.17.0
+	 */
+	PositionEncodings []PositionEncodingKind `json:"positionEncodings"`
+}
+
+type PositionEncodingKind string
+
+const (
+	UTF8  PositionEncodingKind = "utf-8"
+	UTF16 PositionEncodingKind = "utf-16"
+	UTF32 PositionEncodingKind = "utf-32"
+)
 
 type InitializeResponse struct {
 	Response
